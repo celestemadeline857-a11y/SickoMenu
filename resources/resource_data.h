f5c0799caed024650c09b84c4a65cf3285aaf89b
celestemadeline857-a11y <celestemadeline857@gmail.com>
2026-09-06 13:22:09 +0300

    Update resource_data.h

--- resources/resource_data.h
+++ resources/resource_data.h
@@ -18,12 +18,13 @@
 #define IDB_PNG14                       114
 #define IDB_PNG15                       115
 #define IDB_PNG16                       116
+#define IDB_PNG17                       117
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        117
+#define _APS_NEXT_RESOURCE_VALUE        118
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
