9ff0bafbdd20df66cc5f1f98990a110af849f3ca
celestemadeline857-a11y <celestemadeline857@gmail.com>
2026-09-08 17:55:39 +0300

    Fix Cancel Start keybind execution

--- hooks/DirectX.cpp
+++ hooks/DirectX.cpp
@@ -147,6 +147,8 @@ LRESULT __stdcall dWndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPa
         if (KeyBinds::IsKeyPressed(State.KeyBinds.Complete_Tasks) && IsInGame()) CompleteAllTasks();
         if (KeyBinds::IsKeyPressed(State.KeyBinds.Leave_Game) && (IsInGame() || IsInLobby()) && !State.PanicMode)
             app::AmongUsClient_ExitGame((*Game::pAmongUsClient), DisconnectReasons__Enum::ExitGame, NULL);
+        if (KeyBinds::IsKeyPressed(State.KeyBinds.Cancel_Start) && IsInLobby() && State.IsStartCountdownActive)
+            State.CancelingStartGame = true;
     }
     if (KeyBinds::IsKeyPressed(State.KeyBinds.Toggle_Sicko)) {
         State.PanicMode = !State.PanicMode;
